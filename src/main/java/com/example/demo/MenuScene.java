29a506a84174fddb22217fd2ce1c915603028fc8
erict837 <163360343+erict837@users.noreply.github.com>
2024-11-26 11:04:12 -0500

    Added styles.css Redesigned the MenuScene

--- src/main/java/com/example/demo/MenuScene.java
+++ src/main/java/com/example/demo/MenuScene.java
@@ -8,21 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 
 import java.io.FileWriter;
 import java.io.IOException;
 
-
 public class MenuScene extends Application {
     private Stage primaryStage;
-
     private TextField gravityTF = new TextField("9.8");
     private TextField velocityTF = new TextField("3.5");
 
@@ -31,70 +25,63 @@ public class MenuScene extends Application {
         this.primaryStage = primaryStage;
 
         Text flappyTitle = new Text("Realistic Flappy Bird");
-        Font font = Font.font("Arial Black", FontWeight.BOLD, 75);
-        flappyTitle.setFont(font);
-        Button playButton = new Button("PLAY");
-        playButton.setStyle("-fx-font-family: Arial; -fx-font-size: 20px; -fx-font-weight: bold;");
-        playButton.setMinWidth(500);
-        playButton.setMinHeight(100);
+        flappyTitle.getStyleClass().add("title");
 
+        Button playButton = new Button("PLAY");
+        playButton.getStyleClass().add("play-button");
         playButton.setOnAction(actionEvent -> openGameScene());
 
         Label gravityLbl = new Label("Set gravity (m/s^2)");
         Label velocityLbl = new Label("Set velocity (m/s)");
-        gravityLbl.setAlignment(Pos.BASELINE_LEFT);
+        gravityLbl.getStyleClass().add("label");
+        velocityLbl.getStyleClass().add("label");
 
+        gravityTF.getStyleClass().add("text-field");
+        velocityTF.getStyleClass().add("text-field");
 
-
-        GridPane gridPane = new GridPane(); // GridPane for the lbl + txtFields
+        GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
         gridPane.add(gravityLbl, 0, 0);
         gridPane.add(gravityTF, 0, 1);
         gridPane.add(velocityLbl, 1, 0);
         gridPane.add(velocityTF, 1, 1);
-        gridPane.setHgap(10);
-        gridPane.setVgap(1);
+        gridPane.setHgap(20);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(20));
 
-        VBox vBox = new VBox();
-        vBox.getChildren().addAll(flappyTitle, playButton, gridPane);
+        VBox vBox = new VBox(30);
         vBox.setAlignment(Pos.CENTER);
-        vBox.setSpacing(20);
+        vBox.getChildren().addAll(flappyTitle, playButton, gridPane);
         VBox.setMargin(playButton, new Insets(75, 0, 0, 0));
 
         Scene scene = new Scene(vBox, 1600, 800);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
         primaryStage.setTitle("Flappy Bird!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    //.start now stars the primaryStage instead of gameScene
     private void openGameScene() {
         try {
             FileWriter myWriter = new FileWriter("Data.txt");
-            myWriter.write(gravityTF.getText()+"\n"+velocityTF.getText());
+            myWriter.write(gravityTF.getText() + "\n" + velocityTF.getText());
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+
         try {
             GameScene gameScene = new GameScene();
             gameScene.start(primaryStage);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
-
-    public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
 
     public static void main(String[] args) {
         launch();
     }
 }
-
